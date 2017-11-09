b568bb8498c37d3af0f734adeeef6f9fb88c2734
guosiyuan <ruthguosun@163.com>
2017-11-09 18:58:05 +0800

    添加打成war包的配置

--- src/main/java/com/siyuan/Application.java
+++ src/main/java/com/siyuan/Application.java
@@ -2,15 +2,24 @@ package com.siyuan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by ruthguo on 2017/11/7.
  * @author ruthguo
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
+
+
 }
